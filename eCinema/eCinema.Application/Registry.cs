41b7f97ab2eac04258bec1ced77e738445be22e0
Nedim  Alicusic <nedim.alicusic@edu.fit.ba>
2023-04-07 12:10:28 +0200

    Added application project

---
+++ eCinema/eCinema.Application/Registry.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCinema.Application
+{
+    public static class Registry
+    {
+
+        public static void AddApplication(this IServiceCollection services)
+        {
+
+        }
+
+        public static void AddValidators(this IServiceCollection services)
+        {
+
+        }
+    }
+}
