ac2697720e9ad34576af9b177b1269f1a6242ae0
Nedim  Alicusic <nedim.alicusic@edu.fit.ba>
2023-04-10 13:38:24 +0200

    Added api project

---
+++ eCinema/eCinema.Api/Config/ConnectionStringConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace eCinema.Api
+{
+    public class ConnectionStringConfig
+    {
+        public string Main { get; set; } = null!;
+    }
+}
---
+++ eCinema/eCinema.Api/Mapping/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using eCinema.Application;
+using eCinema.Core;
+
+namespace eCinema.Api.Mapping
+{
+    public class UserProfile : BaseProfile
+    {
+        public UserProfile()
+        {
+            CreateMap<AccessSignUpModel, UserUpserDto>()
+                .ForMember(a => a.ProfilePhoto, o => o.Ignore())
+                .ForMember(a => a.Role, o => o.MapFrom(s => Role.User));
+
+        }
+    }
+}
